40958f433a55542747f60ba5f421c228fd37172c
connorfrench <cfrench0803@gmail.com>
2020-09-22 13:45:33 -0400

    Created basic page files

---
+++ front-end/src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+        <h1>
+          hello world
+        </h1>
+    </div>
+  );
+}
+
+export default Home;
