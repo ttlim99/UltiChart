9590fe31029b1a48c9cb7c2719719dd7e34ddff8
jlzeng-i-got-locked-out-of-rokky258 <56747635+jlzeng-i-got-locked-out-of-rokky258@users.noreply.github.com>
2020-09-27 17:16:38 -0400

    Created an employee object and implemented hire()
    The employee object has the same parameters as the one within the mongoDB which should allow for easy creation of new employees

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import db
+import json
 app = Flask(__name__)
+
+class Employee(db.Document):
+    firstName = db.StringField()
+    lastName = db.StringField()
+    companyID = db.IntField()
+    password = db.StringField()
+    positionTitle = db.StringField()
+    companyName = db.StringField()
+    isManager = db.BooleanField()
+    employeeID = db.IntField()
+    managerID = db.IntField()
+    email = db.EmailField()
+    startDate = db.StringField()
+    def to_json(self):
+        return {"firstName": self.firstName,
+                "lastName": self.lastName,
+                "companyID": self.companyID,
+                "password": self.password,
+                "positionTitle": self.positionTitle,
+                "companyName": self.companyName,
+                "isManager": self.isManager,
+                "employeeID": self.employeeID,
+                "managerID": self.managerID,
+                "email": self.email,
+                "startDate": self.startDate}
 
 @app.route('/')
 def flask_mongodb_atlas():
@@ -10,6 +36,24 @@ def flask_mongodb_atlas():
 def test():
     db.db.collection.insert_one({"name": "John"})
     return "Connected to the data base!"
+
+
+@app.route('/', methods=['PUT'])
+def hire():
+    record = json.loads(request.data)
+    employee = Employee(firstName=record['firstName'],
+                lastName=record['lastName'],
+                companyID = record['companyID'],
+                password = record['password'],
+                positionTitle = record['positionTitle'],
+                companyName = record['companyName'],
+                isManager = record['isManager'],
+                employeeID = record['employeeID'],
+                managerId = record['managerID'],
+                email = record['email'],
+                startDate = record['startDate'])
+    employee.save()
+    return jsonify(employee.to_json())
 
 if __name__ == '__main__':
     app.run(port=8000)
