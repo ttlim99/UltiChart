7bf15f0787b519b38c7efd028d6726310c8f5538
connorfrench <cfrench0803@gmail.com>
2020-09-30 09:48:54 -0400

    fixed navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './Home'
 import Login from './Login'
@@ -12,20 +12,24 @@ import './App.css';
 
 function App() {
   return (
+    <div>
     <Router>
-      <Navbar>
-        <Navbar.Brand href="/">UltiChart</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Link to="/">Home</Link>
-          <Link to="/requests">Requests</Link>
-          <Link to="/login">Login</Link>
+      <Navbar className ="navbar-bgColor"  variant = "dark">
+        <Navbar.Brand href="/" >UltiChart</Navbar.Brand>
+        <Nav className = "navbar-main">
+          <Nav.Link href = "/">Home</Nav.Link>
+          <Nav.Link href = "/login">Login</Nav.Link>
+          <Nav.Link href = "/requests">Requests</Nav.Link>
         </Nav>
       </Navbar>
-      <Route path="/" exact render={() => <Home />} />
-      <Route path="/login" render={() => <Login />} />
-      <Route path="/requests" render={() => <Requests />} />
+      <Switch>
+        <Router exact path = "/"><Home/></Router>
+        <Router path = "/login"><Login/></Router>
+        <Router path = "/requests"><Requests/></Router>
+      </Switch>
     </Router>
-  )
-};
+  </div>
+);
+}
 
 export default App;
