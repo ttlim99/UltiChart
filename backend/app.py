37434f93a01ba04cc3daa06e815b296266aafbeb
Jiafeng-Li95 <jeff1106593594@gmail.com>
2020-10-17 22:10:07 -0400

    Merge backend and frontend project.
    merge and run the app in the address of http://127.0.0.1:5000/.

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import jsonify
+import flask
+from flask import Flask, request, jsonify
 import pymongo
 from pymongo import MongoClient
 import json
@@ -9,12 +9,18 @@ from bson import json_util
 # Talking Points for Presentation: 1. Use closest cluster for lowest latency 2. Chose PyMongo over MongoEngine -- why? 3. Demo Login
 # Notes: 3.7.6 but using Python 3.4 or later -- why?
 
-cluster_320 = MongoClient("mongodb://cc320:cc320@320-shard-00-00.8rfoj.mongodb.net:27017,320-shard-00-01.8rfoj.mongodb.net:27017,320-shard-00-02.8rfoj.mongodb.net:27017/320?ssl=true&replicaSet=atlas-mkdts8-shard-0&authSource=admin&retryWrites=true&w=majority")
-
+# Change connection string depending on your python version. 
+cluster_320 =  MongoClient("mongodb://cc320:cc320@320-shard-00-00.8rfoj.mongodb.net:27017,320-shard-00-01.8rfoj.mongodb.net:27017,320-shard-00-02.8rfoj.mongodb.net:27017/320?ssl=true&replicaSet=atlas-mkdts8-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = cluster_320["Employees"]
 collection = db["Tiger_Microsystems-employees"]
 
 app = Flask(__name__)
+
+# Should load all the data into Postman.
+@app.route("/employees", methods=["GET"])
+def get_all_employees():
+    all_employees = list(collection.find({}))
+    return json.dumps(all_employees, default=json_util.default)
 
 @app.route("/details/<email>", methods=["GET"])
 def get_employees(email):
@@ -29,6 +35,52 @@ def get_employees(email):
     all_employees = collection.find({"managerId": current_emp["employeeId"]})
 
     for employee in all_employees:
-        direct_reports.append({"firstName": employee["firstName"], "lastName": employee["lastName"]})
+        direct_reports.append({"firstName": employee["firstName"], "lastName": employee["lastName"], "employeeID": employee["employeeId"], "managerID": employee["managerId"]})
     
     return jsonify({"employeeId": current_emp["employeeId"], "directReports": direct_reports})
+
+# Get the data from the Mongo Server.
+
+# Input data in json format...
+
+# firstName       String
+# lastName        String
+# companyId       Integer
+# password        String      // Don't need yet. Or do we...
+# positionTitle   String
+# companyName     String
+# isManager       Boolean     // False if checked "employee" on form, else true.
+# employeeId      Integer
+# managerId       Integer
+# email           String
+# startDate       String
+
+# Add json data via Postman to server. 
+@app.route("/hire", methods=["POST"])
+def get_json_employees():
+    all_employees = list(collection.find({}))
+    if request.is_json:
+        # Get the data that is being added.
+        employees = request.get_json()
+        # Append the json data to the database.
+        all_employees.append(employees)
+        # Inserts to the database.
+        collection.insert_one(employees);
+        return {'id': len(all_employees)}, 200
+    # The user did not enter json format.
+    else:
+        # The frontend will be notified of the error.
+        flash('data is not in json format')
+        # Return error 400.
+        return render_template('error.html'), 400
+
+@app.route("/") 
+def my_index():
+    return flask.render_template("index.html",token="Hello, Flask and React") 
+@app.route("/login") 
+def my_index2():
+    return flask.render_template("index.html",token="Hello, Flask and React")    
+@app.route("/requests") 
+def my_index3():
+    return flask.render_template("index.html",token="Hello, Flask and React")      
+app.run(debug=True)
---
+++ backend/static/react/precache-manifest.429fdab616409b6139255a515ad0b103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d81a9626dd94dcedabe5bccf16d8d2",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "8eecab0237dad54ac174",
+    "url": "/static/react/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "d449b12c9ac6bc53d806",
+    "url": "/static/react/css/main.c6591f43.chunk.css"
+  },
+  {
+    "revision": "8eecab0237dad54ac174",
+    "url": "/static/react/js/2.416817ab.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/react/js/2.416817ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d449b12c9ac6bc53d806",
+    "url": "/static/react/js/main.c20e9f58.chunk.js"
+  },
+  {
+    "revision": "2b35f277c2abb0edda03",
+    "url": "/static/react/js/runtime-main.d8763b2a.js"
+  }
+]);
